9f4ca0292c522af83ad5c601503771308b48fac6
Cruz3196 <cruzjos9619@gmail.com>
2024-07-19 18:51:54 -0700

    Add project files

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -8,88 +8,104 @@ const NewsletterContainer = styled.div`
   align-items: center;
   background-color: #000000;
   padding: 60px;
-  width: 100%;
-  box-sizing: border-box;
 
   @media (max-width: 768px) {
-    padding: 40px 20px;
+    padding: 40px 30px 40px 30px;
   }
 `;
 
 const NewsletterTitle = styled.h1`
-  font-size: 2.5em;
+  font-size: 40px;
   font-weight: bold;
   color: #fff;
   margin-bottom: 10px;
   text-align: center;
-
   @media (max-width: 768px) {
-    font-size: 1.5em;
+    font-size: 1.4em;
   }
 `;
 
 const NewsletterSubtitle = styled.span`
-  font-size: 1.5em;
+  font-size: 25px;
   color: #fffcf8;
-  text-align: center;
-  margin-bottom: 20px;
 
   @media (max-width: 768px) {
-    font-size: 1em;
-    margin-bottom: 10px;
+    font-size: 15px;
+    text-align: center;
+    margin-bottom: 0px;
   }
 `;
 
 const NewsletterForm = styled.form`
   display: flex;
   margin-top: 20px;
-  width: 100%;
-  max-width: 500px;
+  width: 474px;
   height: 67px;
   background: #ffffff;
   border-radius: 10px;
   align-items: center;
   justify-content: space-between;
-  padding: 0 10px;
 
   @media (max-width: 768px) {
-    height: 50px;
-    padding: 0 5px;
+    height: 40px;
+    width: 95%;
   }
 `;
 
 const NewsletterInput = styled.input`
   padding: 10px;
-  width: 70%;
+  width: 350px;
   border: none;
-  padding-left: 20px;
-  font-size: 1em;
+  padding-left: 30px;
+  font-size: 18px;
   outline: none;
-  flex-grow: 1;
 
   @media (max-width: 768px) {
-    padding: 5px;
-    font-size: 0.9em;
+    width: 70%;
+    font-size: 12px;
+    margin-left: 5px;
   }
 `;
 
 const NewsletterButton = styled.button`
-  width: 30%;
+  width: 110px;
   height: 90%;
-  font-size: 1em;
+  font-size: 18px;
   background: #000000;
   border-radius: 8px;
   padding: 10px;
   color: #fff;
   cursor: pointer;
-  margin-left: 5px;
+  margin-right: 5px;
   text-align: center;
-  border: none;
 
   @media (max-width: 768px) {
-    font-size: 0.8em;
-    padding: 5px;
-    margin-left: 2px;
+    width: 25%;
+    font-size: 12px;
+    margin-right: 2px;
+  }
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  flex-wrap: nowrap; /* Prevent wrapping to new lines */
+  overflow-x: auto; /* Allow horizontal scrolling on small screens */
+  margin-top: 20px;
+
+  @media (max-width: 768px) {
+    justify-content: center;
+  }
+`;
+
+const StyledImage = styled.img`
+  width: 200px; /* Adjust width as needed */
+  height: auto;
+  margin-right: 10px; /* Space between images */
+  border-radius: 10px;
+
+  @media (max-width: 768px) {
+    width: 150px; /* Adjust width for smaller screens */
+    margin-right: 5px; /* Adjust spacing for smaller screens */
   }
 `;
 
@@ -128,6 +144,11 @@ const Newsletter = () => {
         <NewsletterButton>Subscribe</NewsletterButton>
       </NewsletterForm>
       {visible && <div>You have successfully subscribed!</div>}
+      <ImageContainer>
+        <StyledImage src="image1.jpg" alt="Image 1" />
+        <StyledImage src="image2.jpg" alt="Image 2" />
+        <StyledImage src="image3.jpg" alt="Image 3" />
+      </ImageContainer>
     </NewsletterContainer>
   );
 };
